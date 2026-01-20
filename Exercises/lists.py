e677ef43b12b32c9fab241d146d863120733ac10
monica-villanueva <63138869+monica-villanueva@users.noreply.github.com>
2026-01-20 13:03:32 -0800

    add list example

---
+++ Exercises/lists.py
@@ -0,0 +1,10 @@
+# Create a variable and assign it a list that contains an integer, a float, a Boolean value, a string, and a list of 3 integers.
+# Create another variable and assign it a call of the list() function with a string as its argument.
+# Use the keyword "in" to check if the letter "e" is in the list assigned to the variable from step 2 and print the result.
+# Use the keyword "not in" to check if the letter "a" is not in the list assigned to the variable from step 2 and print the result.
+
+mixed_list = [1, 2.1, True, "monica", [3, 2, 1]]
+make_list = list("villanueva")
+
+print("e" in make_list) # True
+print("a" not in make_list) # False
